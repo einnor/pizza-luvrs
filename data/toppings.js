000fe48fd0d39a40efa66f56221ccb42a099f84d
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-10-09 17:10:01 +0300

    Fetch all toppings

--- data/toppings.js
+++ data/toppings.js
@@ -1,7 +1,6 @@
 const Topping = require('../models/topping')
 const { sortBy, values } = require('lodash')
-
-const toppings = {}
+const DynamoStore = require('./dynamoStore')
 
 function init () {
   create('Dough Crust', 'dough_crust.png', 'dough_crust.png', 1)
@@ -19,8 +18,8 @@ function init () {
 }
 
 async function getAll () {
-  const tops = values(toppings)
-  return sortBy(tops, ['order'])
+  const toppings = await DynamoStore.getAllItems('toppings')
+  return sortBy(toppings, ['order'])
 }
 
 async function create (name, previewImage, image, order) {
